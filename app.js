44993617703cf359b9e46fd07805dbbacb3147c4
Okami <omarzerpa316@gmail.com>
2023-06-17 19:55:37 -0300

    progress with the project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3500;
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
+app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`));
