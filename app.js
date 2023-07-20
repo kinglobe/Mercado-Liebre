b55335e2d5cfa4591bc53b371ca190df4ff81197
Okami <omarzerpa316@gmail.com>
2023-07-19 23:25:44 -0300

    agrega-formularios

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.use(express.static('public'));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 
 app.listen(PORT, () => console.log(`server running in http://localhost:${PORT}`));
+
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
